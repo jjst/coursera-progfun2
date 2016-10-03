04bc8708dc6e551574e1435eb1e82db3625260d6
Jeremie Jost <jeremiejost@gmail.com>
2016-10-03 18:34:12 +0100

    Start working on week 3 assignment

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,15 +1,21 @@
 package quickcheck
 
-import common._
-
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen._
+import org.scalacheck.Prop._
 import org.scalacheck._
-import Arbitrary._
-import Gen._
-import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genEmptyHeap: Gen[H] = const(empty)
+
+  lazy val genNonEmptyHeap: Gen[H] = for {
+    v <- arbitrary[A]
+    heap <- oneOf(genEmptyHeap, genEmptyHeap)
+  } yield insert(v, heap)
+
+  lazy val genHeap: Gen[H] = oneOf(genEmptyHeap, genNonEmptyHeap)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
@@ -17,4 +23,20 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("inserting any two elements into empty heap should return minimum") = forAll { (elem1: Int, elem2: Int) =>
+    val min = Math.min(elem1, elem2)
+    val heap = insert(elem2, insert(elem1, empty))
+    findMin(heap) == min
+  }
+
+  property("deleting minimum from one-element heap should result in empty heap") = forAll { (elem: Int) =>
+    isEmpty(deleteMin(insert(elem, empty)))
+  }
+
+  property("finding the minimum of 2 merged heaps should return the minimum of one of them") = forAll { (heap1: H, heap2: H) =>
+      val min1 = findMin(heap1)
+      val min2 = findMin(heap2)
+      val meldedMin = findMin(meld(heap1, heap2))
+      meldedMin == min1 || meldedMin == min2
+  }
 }
