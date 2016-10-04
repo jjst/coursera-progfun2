78b3649ad7ef5f4b3ec6a6616ac712586cb48697
Jeremie Jost <jeremiejost@gmail.com>
2016-10-04 15:58:48 +0100

    More tests. Not there yet :-(

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -28,7 +28,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     }
   }
 
-  property("gen1") = forAll { (h: H) =>
+  property("inserting new minimum should return this minimum") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
@@ -40,7 +40,12 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   }
 
   property("deleting minimum from one-element heap should result in empty heap") = forAll { (elem: Int) =>
-    isEmpty(deleteMin(insert(elem, empty)))
+    isEmpty(deleteMin(insert(elem, empty))) == true
+  }
+
+  property("constantly finding and deleting the minima should give sorted list") = forAll { heap: H =>
+    val elems = popAll(heap)
+    elems == elems.sorted
   }
 
   property("finding the minimum of 2 merged heaps should return the minimum of one of them") = forAll { (heap1: H, heap2: H) =>
@@ -53,12 +58,25 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     !isEmpty(insert(elem, heap))
   }
 
-  property("constantly finding and deleting the minima should give sorted list") = forAll { heap: H =>
-    val elems = popAll(heap)
-    elems == elems.sorted
-  }
-
   property("melded heaps should give back elements from 2 original lists") = forAll { (h1: H, h2: H) =>
     popAll(meld(h1, h2)) == (popAll(h1) ++ popAll(h2)).sorted
+  }
+
+  property("melding with empty list should make no difference") = forAll { h: H =>
+    meld(h, empty) == h
+  }
+
+  property("inserting and deleting minimum duplicate should make no difference") = forAll(genNonEmptyHeap) { h: H =>
+    val min = findMin(h)
+    val newMin = findMin(deleteMin(insert(min, h)))
+    min == newMin
+  }
+
+  property("deleting minimum from 2 or more-element heap should result in non-empty heap") = forAll { (h: H, e1: Int, e2: Int) =>
+    !isEmpty(deleteMin(insert(e2, insert(e1, h))))
+  }
+
+  property("deleting item should result in smaller heap") = forAll(genNonEmptyHeap) { h: H =>
+    popAll(h).size == popAll(deleteMin(h)).size + 1
   }
 }
